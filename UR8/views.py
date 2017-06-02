6536f04901775ce716ec94aed750029c18f159a9
Panos Efstathiadis <visualbasic008@gmail.com>
2017-06-02 19:26:14 +0300

    css

--- UR8/views.py
+++ UR8/views.py
@@ -502,8 +502,7 @@ def upld_vid(request):
                 fileDir = os.path.dirname(os.path.realpath('__file__'))
                 filename = os.path.join(fileDir, 'uploads/media/videos/'+s_vid)
                 clip = VideoFileClip(filename)
-                clip = (clip.fx(vfx.resize, width=1280, height=720))
-                clip.write_videofile(filename)
+                clip.write_videofile(filename,preset = 'placebo')
                 s_vid = s_vid[:-4] + ".jpeg"
                 fileDir = os.path.dirname(os.path.realpath('__file__'))
                 clip.save_frame((os.path.join(fileDir, 'uploads/media/thumpnails/'+s_vid)), t=2)
