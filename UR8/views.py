f6e2c782090b3354c78fac9ed21ac41183fd40f7
Panos Efstathiadis <visualbasic008@gmail.com>
2017-06-04 21:10:14 +0300

    404

--- UR8/views.py
+++ UR8/views.py
@@ -10,7 +10,22 @@ from moviepy.editor import *
 import os
 import json
 import time
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
 def home(request):
     videos = Video.objects.all()
