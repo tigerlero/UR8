675b7ae451f87e8436e8a1eef61190dfc7999c3a
Panagiotis Efstathiadis <visualbasic008@gmail.com>
2023-08-10 01:29:08 +0300

    htmx django settings
    htmx in django settings

--- UR8/settings.py
+++ UR8/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     'django.contrib.staticfiles',
-    'ur8_app'
+    'ur8_app',
+    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "UR8.urls"
